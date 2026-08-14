b7b988bc20bba057008ed2369721d2f7a356ab1d
Gator-Go <gator@sw-builder.com>
2026-08-14 10:27:15 -0400

    documentation changes

--- DroidBuilder.groovy
+++ DroidBuilder.groovy
@@ -48,7 +48,10 @@ def insertAppNames(String theText, def configs) {
 def insertAppCopyrights(String theText, def configs) {
     def result = theText
     configs.appNames.appCopyright.each { n ->
-        result = result.replace(n.'@oldCopyright' as String, n.'@newCopyright' as String)
+        def newCopyright = (n.'@newCopyright' as String)
+            .replace('\\n', System.lineSeparator())
+
+        result = result.replace(n.'@oldCopyright' as String, newCopyright)
     }
     result
 }
